18461c52475e8950b8440b8929c17f85d23a8a5a
jamesdurai <jamesdurai@outlook.com>
2023-06-01 10:08:34 +0530

    Array Delete

---
+++ main.js
@@ -0,0 +1,21 @@
+let arr =[1,2,2,3,4,5,6,6,7,8,4,5]
+let user = parseInt(prompt("Enter the Index value"))
+
+function deleteIndex(arr,user){
+    let result =[];
+    for (i=0; i<arr.length; i++){
+        let exists= false;
+        for(j=0;j<result.length;j++){
+            if(arr[i]===user){
+                exists=true;
+                break;
+            }
+        }
+        if (!exists){
+            result.push(arr[i]);
+        }
+    }
+    return result;
+}
+
+document.write(deleteIndex(arr,user));
